23dc11183f0e51cac44fc98ae6a25030821b4746
RAsya-satya <sae121222@gmail.com>
2025-03-13 09:44:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const ikon = document.getElementById("ikon");
+const navbar = document.getElementById("navbar");
+
+ ikon.addEventListener("click", () => { 
+   navbar.classList.toggle("NAV-LIST");
+ });
